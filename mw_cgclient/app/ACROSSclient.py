71ebe3a2095a4aefaf5c3729612134fbd4aa68f6
Javier Nuñez <javier.nunezfuente.practicas@telefonica.com>
2023-09-13 12:38:31 +0200

    client fix

--- mw_cgclient/app/ACROSSclient.py
+++ mw_cgclient/app/ACROSSclient.py
@@ -3,7 +3,7 @@ import time
 import random
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("localhost", 8888))
+client_socket.connect(("cgserver", 8888))
 response = client_socket.recv(1024)
 print(response.decode())
 multiplier = 10000 # Adjust the multiplier as needed
@@ -12,7 +12,7 @@ print(f"multiplier set to: {multiplier}")
 while True:
     # Simulate cheetah flow burst
     if random.random() < 0.2: # Adjust the probability as needed
-        cheetah_data = b"Cheetah burst data!\n" * (multiplier * random.randrange(0.5, 1.0)) # Generate a burst of data
+        cheetah_data = b"Cheetah burst data!\n" * int(multiplier * random.uniform(0.5, 1.0)) # Generate a burst of data
         client_socket.send(cheetah_data)
         print("Cheetah flow burst sent.")
     time.sleep(random.uniform(0.5, 2)) # Random interval between bursts
