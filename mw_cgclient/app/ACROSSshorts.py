0911882ca1a0616c3b4df91563f8d66d8f1dfabb
Javier Nuñez <javier.nunezfuente.practicas@telefonica.com>
2023-09-14 12:45:16 +0200

    root_login

--- mw_cgclient/app/ACROSSshorts.py
+++ mw_cgclient/app/ACROSSshorts.py
@@ -4,7 +4,7 @@ import random
 def simulate_cheetah_flow():
 
     driver = webdriver.Firefox(executable_path="/root/geckodriver")
-    driver.get("https://www.youtube.com/shorts")
+    driver.get("https://www.tiktok.com/es/")
     
     while True:
         driver.execute_script("window.scrollBy(0, 1000);") # Adjust the amount of pixels to scroll vertically in order to skip to the next video in the feed
