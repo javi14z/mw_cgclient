fcaf6286e919b1c02f02317b94406d7b06015071
Javier Nuñez <javier.nunezfuente.practicas@telefonica.com>
2023-09-15 14:31:21 +0200

    update

--- mw_cgclient/app/ACROSSconsuming_video.py
+++ mw_cgclient/app/ACROSSconsuming_video.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 import time
 
 def simulate_user():
-    driver = webdriver.Firefox(executable_path="/root/geckodriver")
+    driver = webdriver.Firefox(executable_path="/root/geckodriver") # Use the appropriate driver
     driver.get("https://ia801802.us.archive.org/15/items/alexander-the-great-1997-part-1/Alexander%20The%20Great%20%281997%29%20Part%201.mp4")
-    # Simula la visualización
-    time.sleep(3600)  # Deja que el video se reproduzca durante un tiempo
+    # Simulate the viewing
+    time.sleep(3600)  # Let the video play for a while  
     driver.quit()
 
 simulate_user()
