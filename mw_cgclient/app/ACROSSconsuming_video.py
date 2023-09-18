e584caeb489aded5bfb3821e7d5a1e52fb46eb10
Javier Nuñez <javier.nunezfuente.practicas@telefonica.com>
2023-09-18 10:48:12 +0200

    youtube shorts fix

--- mw_cgclient/app/ACROSSconsuming_video.py
+++ mw_cgclient/app/ACROSSconsuming_video.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 def simulate_user():
     driver = webdriver.Firefox(executable_path="/root/geckodriver") # Use the appropriate driver
     driver.get("https://ia801802.us.archive.org/15/items/alexander-the-great-1997-part-1/Alexander%20The%20Great%20%281997%29%20Part%201.mp4")
+
     # Simulate the viewing
     time.sleep(3600)  # Let the video play for a while  
     driver.quit()
--- mw_cgclient/app/ACROSSshorts.py
+++ mw_cgclient/app/ACROSSshorts.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 import random
 def simulate_cheetah_flow():
 
     driver = webdriver.Firefox(executable_path="/root/geckodriver")
     driver.get("https://www.youtube.com/shorts")
-    
+
+    # Accept cookies
+    time.sleep(5)
+    agree_button_xpath = "/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/form[2]/div/div/button/span"
+    agree_button = driver.find_element(By.XPATH, agree_button_xpath)
+    agree_button.click()
+    time.sleep(5)
+
+   
     while True:
-        driver.execute_script("window.scrollBy(0, 1000);") # Adjust the amount of pixels to scroll vertically in order to skip to the next video in the feed
-        time.sleep(random.uniform(0.5, 2)) # Random interval between scrolls
+        next_button_xpath = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[4]/div[2]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]"
+        next_button = driver.find_element(By.XPATH, next_button_xpath)
+        next_button.click()
+        time.sleep(random.uniform(1.5, 5)) # Random interval between scrolls
         
 simulate_cheetah_flow()
