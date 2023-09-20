2a9afc84319ff01e118d0ae4c35374873935056b
Javier Nuñez <javier.nunezfuente.practicas@telefonica.com>
2023-09-20 13:27:38 +0200

    sudo added in all dockerfiles, nginx configuration

---
+++ mw_ddosclient/app/vegeta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ejecuta la prueba de carga y guarda los resultados en results.bin
+sudo vegeta attack -targets=vegeta-config.txt -duration=100s | tee results.bin
+
+# Genera un informe JSON a partir de los resultados
+vegeta report -type=json results.bin > metrics.json
+
+# Genera un gráfico HTML a partir de los resultados
+cat results.bin | vegeta plot > plot.html
+
+# Genera un informe de histograma a partir de los resultados
+cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
