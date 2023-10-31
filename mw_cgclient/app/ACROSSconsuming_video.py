677e523d5daaf1a516e78e79692a24db296b19ad
Javier Nuñez <javi14jul@gmail.com>
2023-10-31 16:41:39 +0100

    Change firefox for chromedriver & added log

--- mw_cgclient/app/ACROSSconsuming_video.py
+++ mw_cgclient/app/ACROSSconsuming_video.py
@@ -1,14 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
+import sys
+import json
+import os
+import numpy
 
-def simulate_user():
 
+def simulate_user(duration, netlog_name):
+
+    # Configuration
     options = Options()
-    options.headless = True
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--log-net-log=' + netlog_name)
+    options.set_capability('goog:loggingPrefs', {'performance': 'ALL', "browser": "ALL"})
 
-    driver = webdriver.Firefox(executable_path="/root/geckodriver", options=options) # Use the appropriate driver
+    caps = DesiredCapabilities.CHROME
+    caps['loggingPrefs'] = {'performance': 'ALL'}
+    caps['pageLoadStrategy'] = 'normal'
+
+    driver = webdriver.Chrome(executable_path="/root/chromedriver", options=options, desired_capabilities=caps, service_args=["--verbose", 
+    f"--log-path=/home/cognet/chrome_logs/yt_driver.{os.path.basename(netlog_name)}.log"]) # Use the appropriate driver
+
     # Open youtube
     driver.get("https://www.youtube.com/watch?v=-No-226O0tg")
 
@@ -22,8 +39,79 @@ def simulate_user():
     time.sleep(5)
     driver.find_element(By.TAG_NAME, 'body').send_keys("K")
 
-    # Simulate the viewing
-    time.sleep(3600)  # Let the video play for a while  
+    # Simulate the viewing for the specified duration
+    time.sleep(duration)
+    # Get the log
+    log_performance = driver.get_log('performance')
+
+    #Save log in .json
+    with open('/home/cognet/chrome_logs/netlog.' + netlog_name +'.json', 'w') as log_file:
+        for entry in log_performance:
+            log_file.write(json.dumps(entry) + '\n')
+
+    # last_known_url = None
+    # request_headers_data = []
+    # response_headers_data = []
+
+    # for entry in log_performance:
+    #     try:
+    #         obj_serialized = entry.get("message")
+    #         obj = json.loads(obj_serialized)
+    #         message = obj.get("message")
+    #         method = message.get("method")
+    #         url = message.get("params", {}).get("documentURL")
+
+    #         # Update last known URL if available
+    #         if url:
+    #             last_known_url = url
+
+    #         if method == 'Network.requestWillBeSentExtraInfo' or method == 'Network.requestWillBeSent':
+    #             try:
+    #                 request_payload = message['params'].get('request', {})
+    #                 request_headers = request_payload.get('headers', {})
+    #                 # Store request headers and last known URL in request_headers_data
+    #                 request_headers_data.append({"url": last_known_url, "headers": request_headers})
+    #             except KeyError:
+    #                 pass
+
+    #         if method == 'Network.responseReceivedExtraInfo' or method == 'Network.responseReceived':
+    #             try:
+    #                 response_payload = message['params'].get('response', {})
+    #                 response_headers = response_payload.get('headers', {})
+    #                 # Store response headers and last known URL in response_headers_data
+    #                 response_headers_data.append({"url": last_known_url, "headers": response_headers})
+    #             except KeyError:
+    #                 pass
+
+    #         if method == 'Network.loadingFinished':
+    #             # Network request is finished, you can now access request_headers_data and response_headers_data
+    #             print("Request Headers:")
+    #             for request_data in request_headers_data:
+    #                 print("URL:", request_data["url"])
+    #                 print(request_data["headers"])
+    #             print("Response Headers:")
+    #             for response_data in response_headers_data:
+    #                 print("URL:", response_data["url"])
+    #                 print(response_data["headers"])
+    #             print('--------------------------------------')
+    #     except Exception as e:
+    #         raise e from None
+
     driver.quit()
 
-simulate_user()
+#--------------------------------------------------------------------------------------------------
+
+netlog_name = str(os.getpid()) + "_" + str(numpy.float64(time.time()))
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 ACROSSconsuming_video.py <duration>")
+    exit(1)
+
+try:
+    duration = int(sys.argv[1])
+    simulate_user(duration, netlog_name)
+except ValueError:
+    print("The argument value must be a valid integer.")
+    exit(1)
+
--- mw_cgclient/app/ACROSSshorts.py
+++ mw_cgclient/app/ACROSSshorts.py
@@ -1,14 +1,32 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+import sys
+import json
+import os
+import numpy
 import time
 import random
-def simulate_cheetah_flow():
 
+def simulate_cheetah_flow(duration, netlog_name):
+
+    # Configuration
     options = Options()
-    options.headless = True
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--log-net-log=' + netlog_name)
+    options.set_capability('goog:loggingPrefs', {'performance': 'ALL', "browser": "ALL"})
 
-    driver = webdriver.Firefox(executable_path="/root/geckodriver", options=options) # Use the appropriate driver
+    caps = DesiredCapabilities.CHROME
+    caps['loggingPrefs'] = {'performance': 'ALL'}
+    caps['pageLoadStrategy'] = 'normal'
+
+    driver = webdriver.Chrome(executable_path="/root/chromedriver", options=options, desired_capabilities=caps, service_args=["--verbose", 
+    f"--log-path=/home/cognet/chrome_logs/short_driver.{os.path.basename(netlog_name)}.log"]) # Use the appropriate driver
+
     driver.get("https://www.youtube.com/shorts")
 
     # Accept cookies
@@ -18,11 +36,33 @@ def simulate_cheetah_flow():
     agree_button.click()
     time.sleep(5)
 
-    #Click on scroll short
-    while True:
-        next_button_xpath = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[4]/div[2]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]"
-        next_button = driver.find_element(By.XPATH, next_button_xpath)
-        next_button.click()
+    start_time = time.time()
+    #Scroll short
+    while  time.time() - start_time < duration:
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ARROW_DOWN)
         time.sleep(random.uniform(1.5, 5)) # Random interval between scrolls
-        
-simulate_cheetah_flow()
+
+        # Get the log
+    log_performance = driver.get_log('performance')
+
+    #Save log in .json
+    with open('/home/cognet/chrome_logs/netlog.' + netlog_name +'.json', 'w') as log_file:
+        for entry in log_performance:
+            log_file.write(json.dumps(entry) + '\n')
+
+
+    driver.quit()
+
+netlog_name = str(os.getpid()) + "_" + str(numpy.float64(time.time()))
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 ACROSSACROSSshorts.py <duration>")
+    exit(1)
+
+try:
+    duration = int(sys.argv[1])
+    simulate_cheetah_flow(duration, netlog_name)
+except ValueError:
+    print("The argument value must be a valid integer.")
+    exit(1)
