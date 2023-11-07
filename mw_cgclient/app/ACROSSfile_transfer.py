c914010059c1c47341956cf48ce7b974d8cbf486
Javier Nuñez <javi14jul@gmail.com>
2023-11-07 13:36:17 +0100

    file_tranfer URL added

--- mw_cgclient/app/ACROSSfile_transfer.py
+++ mw_cgclient/app/ACROSSfile_transfer.py
@@ -1,9 +1,19 @@
 import os
+import sys
 
 def simulate_wget_download(url, save_path):
     command = f"wget {url} -O {save_path} -o /home/cognet/chrome_logs/file_transfer.log"
     os.system(command)
 
-video_url = "https://ia801802.us.archive.org/15/items/alexander-the-great-1997-part-1/Alexander%20The%20Great%20%281997%29%20Part%201.mp4"
-save_path = "downloaded_video.mp4"
-simulate_wget_download(video_url, save_path)
+save_path = "file_transfer.mp4"
+
+if len(sys.argv) != 2:
+    print("Usage: python3 ACROSSfile_transfer.py <URL>")
+    exit(1)
+
+try:
+    video_url = sys.argv[1]
+    simulate_wget_download(video_url, save_path)
+except ValueError:
+    print("The argument value must be a valid integer.")
+    exit(1)
